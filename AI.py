d3ccee4acd7c194bd2ef1207a88d522199e6f64b
sam <sameh.nour.uni@outlook.com>
2023-12-23 20:27:25 +0100

    H

--- AI.py
+++ AI.py
@@ -3,7 +3,7 @@ Created on Dec 3, 2016
 
 @author: Sam Ragusa
 '''
-
+import numpy as np
 import random
 import json
 from ast import literal_eval
@@ -96,57 +96,98 @@ class Q_Learning_AI(Player):
         self.learning_rate = the_learning_rate
     
 
-    def get_states_from_boards_spots(self, boards_spots):
+    def get_states_from_boards_positions(self, game_board_positions):
         """
         Gets an array of tuples from the given set of board spots,
         each tuple representing the characteristics which define the
         state the board is in. 
-        
+
         Format of returned data:
-        [(own_pieces, opp_pieces, own_kings, opp_kings, own_edges, own_vert_center_mass, opp_vert_center_mass), ...]
+        a lower vertical center of mass could indicate that a player's pieces are more defensive, 
+        while a higher vertical center of mass could indicate a more aggressive or advanced position.
+        [(own_pieces, opp_pieces, own_kings, OP_kings, own_edges, MY_V_COM, OP_V_COM), ...]
         """
-        piece_counters = [[0,0,0,0,0,0,0] for j in range(len(boards_spots))] 
-        for k in range(len(boards_spots)):
-            for j in range(len(boards_spots[k])):
-                for i in range(len(boards_spots[k][j])):
-                    if boards_spots[k][j][i] != 0:
-                        piece_counters[k][boards_spots[k][j][i]-1] = piece_counters[k][boards_spots[k][j][i]-1] + 1
-                        if (self.player_id and (boards_spots[k][j][i] == 1 or boards_spots[k][j][i] == 3)) or (not self.player_id and (boards_spots[k][j][i] == 2 or boards_spots[k][j][i] == 4)):
-                            if i==0 and j%2==0:
-                                piece_counters[k][4] = piece_counters[k][4] + 1
-                            elif i==3 and j%2==1:
-                                piece_counters[k][4] = piece_counters[k][4] + 1
-                                
-                            piece_counters[k][5] = piece_counters[k][5] + j
-                        else: 
-                            piece_counters[k][6] = piece_counters[k][6] + j
-            
-            if piece_counters[k][0] + piece_counters[k][2] != 0:
-                piece_counters[k][5] = int(piece_counters[k][5] / (piece_counters[k][0] + piece_counters[k][2]))
-            else:
-                piece_counters[k][5] = 0
-            if piece_counters[k][1] + piece_counters[k][3] != 0:
-                piece_counters[k][6] = int(piece_counters[k][6] / (piece_counters[k][1] + piece_counters[k][3]))
-            else:
-                piece_counters[k][6] = 0
+        def is_own_piece(piece):
+            return (self.player_id and (piece == 1 or piece == 3)) or (not self.player_id and (piece == 2 or piece == 4))
+
+        def is_edge_piece(i, j):
+            return (i==0 and j%2==0) or (i==3 and j%2==1)
+
+        def calculate_center_of_mass(piece_counters):
+            for k in range(len(piece_counters)):
+                own_pieces = piece_counters[k][0] + piece_counters[k][2]
+                opp_pieces = piece_counters[k][1] + piece_counters[k][3]
+
+                if own_pieces != 0:
+                    average_own_piece_position = piece_counters[k][5] / own_pieces
+                    piece_counters[k][5] = int(average_own_piece_position)
+                else:
+                    piece_counters[k][5] = 0
+
+                if opp_pieces != 0:
+                    average_opp_piece_position = piece_counters[k][6] / opp_pieces
+                    piece_counters[k][6] = int(average_opp_piece_position)
+                else:
+                    piece_counters[k][6] = 0
+
+        piece_counters = [[0,0,0,0,0,0,0] for _ in range(len(game_board_positions))]
+
+        for k, board in enumerate(game_board_positions):
+            for j, row in enumerate(board):
+                for i, piece in enumerate(row):
+                    if piece != 0:
+                        piece_counters[k][piece-1] += 1
+                        if is_own_piece(piece):
+                            if is_edge_piece(i, j):
+                                piece_counters[k][4] += 1
+                            piece_counters[k][5] += j
+                        else:
+                            piece_counters[k][6] += j
+
+        calculate_center_of_mass(piece_counters)
 
         return [tuple(counter) for counter in piece_counters]
                                  
 
-    def get_desired_transition_between_states(self, possible_state_array, initial_transition_value=10):#%%%%%%%%%%%%%%%%%% FOR (1)
+    def get_optimal_transition_between_states(self, possible_state_array, initial_transition_value=10):#%%%%%%%%%%%%%%%%%% FOR (1)
         """
         Gets the desired transition to taken for the current board configuration.
         If any possible transition does not exist, it will create it.
         """
-        cur_state = tuple(self.get_states_from_boards_spots([self.board.spots])[0])
+        # Get the current state of the board
+        cur_state = tuple(self.get_states_from_boards_positions([self.board.spots])[0])
+
+        # Initialize an empty dictionary to store processed transitions
         done_transitions = {}
-        for state in possible_state_array:#%%%%%%%%%%%%%%%%%%%%%% FOR (1)
-            if done_transitions.get((cur_state, tuple(state))) is None:
-                if self.transitions.get((cur_state, tuple(state))) is None:
-                    self.transitions.update({(cur_state, tuple(state)):initial_transition_value})
-                done_transitions.update({(cur_state, tuple(state)):self.transitions.get((cur_state, tuple(state)))})
-                
-            
+
+        # Iterate over each possible state
+        for state in possible_state_array:
+            # Convert the state to a tuple
+            state_tuple = tuple(state)
+
+            # Create a tuple representing the transition from the current state to the possible state
+            transition = (cur_state, state_tuple)
+
+            # If the transition has not been processed before
+            if transition not in done_transitions:
+                # If the transition does not exist in self.transitions, add it with an initial value
+                if transition not in self.transitions:
+                    self.transitions[transition] = initial_transition_value
+
+                # Add the transition and its value to done_transitions
+                done_transitions[transition] = self.transitions[transition]
+        """
+        When it is the reinforcement learning AI's turn, it looks at a list of possible moves it could make, 
+        and if their respective state transitions are not yet known, it adds them to the dictionary of 
+        transitions. Then it will chose to do one of two things, based on a specified probability. 
+        The first is that it could chose a move at random, this is used during training so that transitions 
+        other then the current highest valued transition will still be trained. If the learning AI does not 
+        move randomly, it will chose the move who's state transition has the highest value associated with it.
+
+        "state transitions" are the pairs of initial and final states resulting from an action. 
+        They are often used in reinforcement learning algorithms to learn which actions are beneficial 
+        in which states, by associating rewards with transitions.
+        """
         if random != 0 and random.random() < self.random_move_probability:
             try:
                 return list(done_transitions.keys())[random.randint(0, len(done_transitions)-1)]
@@ -165,9 +206,10 @@ class Q_Learning_AI(Player):
         Update self.transitions with a completed game before the board
         is cleared.
         """
-        cur_state = self.get_states_from_boards_spots([self.board.spots])[0]
+        cur_state = self.get_states_from_boards_positions([self.board.spots])[0]
         transition = (self.pre_last_move_state ,self.post_last_move_state)
-
+        # ! the game is terminal so the reward is the value of the final state
+        # T(S_n, S_{n+1}) = T(S_n, S_{n+1}) + alpha * (R(S_n, S_{n+1}))
         self.transitions[transition] = self.transitions[transition] + self.learning_rate * reward_function(transition[0],cur_state)
 
         self.pre_last_move_state = None
@@ -179,6 +221,7 @@ class Q_Learning_AI(Player):
         """
         Get an array of of information about the dictionary self.transitions .
         It returns the information in the form:
+        # ! Transition formate
         [num_transitions, num_start_of_transitions, avg_value, max_value, min_value]
         
         NOTES:
@@ -213,13 +256,13 @@ class Q_Learning_AI(Player):
         print("Minimum value for transition: ".ljust(35), info[4])
     
         
-    def save_transition_information(self, file_name="data.json"):
+    def save_game_transitions_to_file(self, file_name="data.json"):
         """
         Saves the current transitions information to a specified
         json file. 
         """
         with open(file_name, 'w') as fp:
-            json.dump({str(k): v for k,v in self.transitions.items()}, fp)
+            json.dump({str(k): v for k,v in self.transitions.items()}, fp, indent= 4)
         
         
     def load_transition_information(self, file_name):
@@ -230,7 +273,7 @@ class Q_Learning_AI(Player):
             self.transitions = {literal_eval(k): v for k,v in json.load(fp).items()}
         
         
-    def get_optimal_potential_value(self, depth):
+    def get_max_future_state_value(self, depth):
         """
         Look ahead a given number of moves and return the maximal value associated 
         with a move of that depth. 
@@ -250,7 +293,7 @@ class Q_Learning_AI(Player):
         2) ONLY WORKS FOR DEPTH OF 1 RIGHT NOW
         """
         answer = float("-inf")
-        cur_state = self.get_states_from_boards_spots([self.board.spots])[0]
+        cur_state = self.get_states_from_boards_positions([self.board.spots])[0]
         for k,v in self.transitions.items():
             if v > answer and k[0] == cur_state:
                 answer = v
@@ -268,37 +311,39 @@ class Q_Learning_AI(Player):
         
         PRECONDITIONS:
         1)  The board exists and is legal
+        self.pre_last_move_state = self.get_states_from_boards_positions([self.board.spots])[0]: 
+        This line is getting the current state of the game board and storing it as the "pre-last-move" state. This state represents the board configuration before the AI makes its move.
         """
         if self.pre_last_move_state is not None:#%%%%%%%%%%%%%%%%%%%%%%% FOR (1)
-            cur_state = self.get_states_from_boards_spots([self.board.spots])[0]
+            cur_state = self.get_states_from_boards_positions([self.board.spots])[0]
     
             transition = (self.pre_last_move_state ,self.post_last_move_state)
             try:# self.transitions.get(transition) is not None:#%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR (1)
-                max_future_state = self.get_optimal_potential_value(1)
+                max_future_state = self.get_max_future_state_value(1)
+                # ! T(S_n, S_{n+1}) = T(S_n, S_{n+1}) + alpha * (R(S_n, S_{n+1}) + gamma * max_a(T(S_{n+1}, S_{n+2})) - T(S_n, S_{n+1}))
                 self.transitions[transition] = self.transitions[transition] + self.learning_rate * (reward_function(transition[0],cur_state)+ self.discount_factor* max_future_state - self.transitions[transition])
             except:#%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR (1)
                 self.transitions[transition] = self.transitions[transition] + self.learning_rate * (reward_function(transition[0],cur_state))
         
-        
-        self.pre_last_move_state = self.get_states_from_boards_spots([self.board.spots])[0]#%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR (1)
-        
-        possible_next_moves = self.board.get_possible_next_moves()
-        possible_next_states = self.get_states_from_boards_spots(self.board.get_potential_spots_from_moves(possible_next_moves))
-        
-        self.post_last_move_state = self.get_desired_transition_between_states(possible_next_states)[1]   
+        # get the current board state
+        self.pre_last_move_state = self.get_states_from_boards_positions([self.board.spots])[0]#%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR (1)
+        # This line is getting all possible moves that the AI could make from the current board state.
+        possible_next_moves = self.board.get_possible_next_moves_from_board()
+        # This line is calculating the potential next states of the game board that would result from each of the possible moves.
+        possible_next_states = self.get_states_from_boards_positions(self.board.get_potential_spots_from_moves(possible_next_moves))
+        # This line is determining the most desirable next state according to the AI's learned values. This state is stored as the 
+        # "post-last-move" state, representing the desired board configuration after the AI makes its move.
+        self.post_last_move_state = self.get_optimal_transition_between_states(possible_next_states)[1]   
         
         considered_moves = []
+        # The for loop is creating a list of all moves that would lead to the post-last-move state. 
+        # It does this by iterating over all possible next states and checking if each one matches 
+        # the post-last-move state. If a match is found, the corresponding move is added to the considered_moves list.
         for j in range(len(possible_next_states)):
             if tuple(possible_next_states[j]) == self.post_last_move_state:
                 considered_moves.append(possible_next_moves[j])
                 
-                
-        #I believe with the updated board.is_game_over() I don't need to use this try statement 
-#         try:
-#             return considered_moves[random.randint(0,len(considered_moves)-1)]
-#         except ValueError:
-#             return []
-        
+        # Pick a random move from the considered moves
         return considered_moves[random.randint(0,len(considered_moves)-1)]
 
 def get_number_of_pieces_and_kings(spots, player_id=None):
@@ -365,7 +410,7 @@ class Alpha_beta(Player):
             if board.player_turn != maximizing_player:
                 return  players_info[1] + 2 * players_info[3] - (players_info[0] + 2 * players_info[2]), None
             return  players_info[0] + 2 * players_info[2] - (players_info[1] + 2 * players_info[3]), None
-        possible_moves = board.get_possible_next_moves()
+        possible_moves = board.get_possible_next_moves_from_board()
 
         potential_spots = board.get_potential_spots_from_moves(possible_moves)
         desired_move_index = None
@@ -495,38 +540,54 @@ def pretty_outcome_display(outcomes):
     print("Max moves made: ".ljust(35), max_moves_made)
     print("Min moves made: ".ljust(35), min_moves_made)
     
-
+    
 def plot_end_game_information(outcome, interval, title="End of Game Results"):
     """
+    Plots the end game results.
     """
-    player1_wins = [0 for _ in range(int(len(outcome)/interval))]
-    player2_wins = [0 for _ in range(int(len(outcome)/interval))]
-    ties = [0 for _ in range(int(len(outcome)/interval))]
-    move_limit = [0 for _ in range(int(len(outcome)/interval))]
-    
-    for j in range(int(len(outcome)/interval)):
-        for i in range(interval):
-            if outcome[j*interval + i][0] == 0:
-                player1_wins[j] = player1_wins[j] + 1
-            elif outcome[j*interval + i][0] == 1:
-                player2_wins[j] = player2_wins[j] + 1
-            elif outcome[j*interval + i][0] == 2:
-                ties[j] = ties[j] + 1
-            else:
-                move_limit[j] = move_limit[j] + 1
-                
-    plt.figure(title)
-    
-    p1_win_graph, = plt.plot(player1_wins, label = "Player 1 wins")
-    p2_win_graph, = plt.plot(player2_wins, label = "Player 2 wins")
-    tie_graph, = plt.plot(ties, label = "Ties")
-    move_limit_graph, = plt.plot(move_limit, label = "Move limit reached")
-    
-    plt.ylabel("Occurance per " +str(interval) + " games")
-    plt.xlabel("Interval")
-    
-    plt.legend(handles=[p1_win_graph, p2_win_graph, tie_graph, move_limit_graph])
+    # Calculate the number of intervals
+    num_intervals = len(outcome) // interval
 
+    # Initialize result lists
+    player1_wins = [0] * num_intervals
+    player2_wins = [0] * num_intervals
+    ties = [0] * num_intervals
+    move_limit = [0] * num_intervals
+
+    # Iterate over each interval
+    for j in range(num_intervals):
+        # Iterate over each game in the interval
+        for i in range(interval):
+            game_result = outcome[j*interval + i][0]
+
+            # Update the appropriate result list based on the game result
+            if game_result == 0:
+                player1_wins[j] += 1
+            elif game_result == 1:
+                player2_wins[j] += 1
+            elif game_result == 2:
+                ties[j] += 1
+            else:
+                move_limit[j] += 1
+
+    bar_width = 0.2
+    intervals = np.arange(num_intervals)
+
+    plt.figure(title)
+
+    plt.bar(intervals - bar_width*1.5, player1_wins, bar_width, label="Player 1 wins")
+    plt.bar(intervals - bar_width/2, player2_wins, bar_width, label="Player 2 wins")
+    plt.bar(intervals + bar_width/2, ties, bar_width, label="Ties")
+    plt.bar(intervals + bar_width*1.5, move_limit, bar_width, label="Move limit reached")
+
+    # Set the labels
+    plt.ylabel(f"Occurrence per {interval} games")
+    plt.xlabel("Interval")
+
+    # Add a legend
+    plt.legend()
+
+    plt.show()
 
 
  
@@ -561,14 +622,14 @@ for j in range(NUM_TRAINING_ROUNDS):
     print("Round " + str(j+1) + " completed!")
     PLAYER1.set_random_move_probability(TRAINING_RANDOM_MOVE_PROBABILITY)
     PLAYER1.set_learning_rate(LEARNING_RATE)
-    #print("")
-    #PLAYER1.print_transition_information(PLAYER1.get_transitions_information())
     print("")
-    PLAYER1.save_transition_information()
+    PLAYER1.print_transition_information(PLAYER1.get_transitions_information())
+    print("")
+    PLAYER1.save_game_transitions_to_file()
  
     
-#plot_end_game_information(training_info, 200, "Training Information")
-#plot_end_game_information(validation_info, NUM_VALIDATION_GAMES, "Validation Information")
+plot_end_game_information(training_info, 200, "Training Information")
+plot_end_game_information(validation_info, NUM_VALIDATION_GAMES, "Validation Information")
 plt.show()
  
 pretty_outcome_display(training_info)
@@ -586,8 +647,8 @@ PLAYER1.print_transition_information(PLAYER1.get_transitions_information())
 print(" ")
 pretty_outcome_display(play_n_games(PLAYER1, PLAYER4, NUM_GAMES_TO_TEST, TESTING_MOVE_LIMIT))
 PLAYER1.print_transition_information(PLAYER1.get_transitions_information())
+PLAYER1.save_game_transitions_to_file()
  
 """
  
-PLAYER1.save_transition_information()
 
